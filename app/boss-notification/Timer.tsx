d96681071ddfd83ad2b59374c41606c403eae11b
tuosm9390 <tuosm123@naver.com>
2023-05-08 07:08:13 +0900

    123

--- app/boss-notification/Timer.tsx
+++ app/boss-notification/Timer.tsx
@@ -1,38 +1,28 @@
 'use client'
 
+import moment from 'moment';
+import 'moment/locale/ko';
 import React, { useState } from "react";
+import Moment from "react-moment";
+import { useInterval } from "use-interval";
 
 const Timer = () => {
-  const [time, setTime] = useState("00:00:00")
+  const [nowTime, setNowTime] = useState(Date.now())
+
   const [hours, setHours] = useState("00")
   const [minutes, setMinutes] = useState("00")
   const [seconds, setSeconds] = useState("00")
-  const [amPm, setAmPm] = useState("AM")
 
-
-  Date.prototype.amPm = function () {
-    let h = this.getHours() < 12 ? "오전" : "오후";
-    return h;
-  }
-
-  const currentTimer = () => {
+  useInterval(() => {
     const date = new Date()
     const hours = String(date.getHours()).padStart(2, "0")
     const minutes = String(date.getMinutes()).padStart(2, "0")
     const seconds = String(date.getSeconds()).padStart(2, "0")
-    const amPm = date.amPm()
-    // setTime(`${hours}:${minutes}:${seconds}`)
-    setAmPm(amPm)
     setHours(`${hours}`)
     setMinutes(`${minutes}`)
     setSeconds(`${seconds}`)
-  }
-
-  const startTimer = () => {
-    setInterval(currentTimer, 1000)
-  }
-
-  startTimer()
+    setNowTime(Date.now())
+  }, 1000)
 
   return (
     <div className="py-6 flex flex-col border-[1px] border-gray-700 w-full rounded-lg mt-4 items-center justify-center">
@@ -43,8 +33,8 @@ const Timer = () => {
         {/* {time} */}
         <span className={`mr-2`}>{hours}</span>:
         <span className={`mx-2`}>{minutes}</span>:
-        <span className={`${seconds >= 50 ? "text-red-500" : ""} ml-2`}>{seconds}</span>
-        {<span className="ml-2 font-light text-lg">{amPm}</span>}
+        <span className={`${parseInt(seconds) >= 50 ? "text-red-500" : ""} ml-2`}>{seconds}</span>
+        {moment().format("a")}
       </div>
     </div>
   );

222fd2eafd24284b0c5b34af910af77ff4782b36
tuosm9390 <tuosm123@naver.com>
2023-05-08 07:23:58 +0900

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
+  basePath: "/talesweaver_homework",
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
